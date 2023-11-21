086b806e55b590de4e529e2ef5c1918b130ff877
tu <bhtu.work@gmail.com>
2023-11-21 10:49:41 +0700

    assure findFirstWithKeywordCount result, guard cant find one

--- src/main/java/org/example/databases/mongo/reposistories/TextWithAllWordCountRepo.java
+++ src/main/java/org/example/databases/mongo/reposistories/TextWithAllWordCountRepo.java
@@ -1,29 +1,44 @@
 package org.example.databases.mongo.reposistories;
 
-import org.example.models.Text;
 import org.example.models.TextWithAllWordCount;
 import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TextWithAllWordCountRepo extends MongoRepository<TextWithAllWordCount, String> {
     /**
      * This method should be used to retrieve `keywordCounts` as it will find
      * a document with not empty `keywordCounts` and `keywordCounts` are maintains
      * across documents
+     *
      * @return a document which contains `keywordCounts`
      */
-    TextWithAllWordCount findFirst();
+    @Aggregation(pipeline = {
+            "{ $match: " +
+                "{" +
+                    // make sure this field exist
+                    "'keywordCounts': { " +
+                        "$exists: true," +
+                        "$ne: null," +
+                        "$not: {$size: 0}" +
+                    " }" +
+                "} " +
+            "}",
+            "{ $limit: 1}"
+    })
+    Optional<TextWithAllWordCount> findFirstWithKeywordCount();
 
     /**
      * Perform a full-text search on document associate with TextWithAllWordCount.
-     * @apiNote This method always return results with `keywordCounts = null` to
-     * speed up search and reduce memory. To access `keywordCounts`, please use
-     * `findFirst`
-     * @param text search phrase
+     *
+     * @param text  search phrase
      * @param limit maximum result expected
      * @return best suited texts with the search phrase in descending order.
+     * @apiNote This method always return results with `keywordCounts = null` to
+     * speed up search and reduce memory. To access `keywordCounts`, please use
+     * `findFirstWithKeywordCount`
      */
     @Aggregation(pipeline = {
             "{ $match: {$text: {$search: '?0', $language: vi } } }",
--- src/main/java/org/example/services/OpenService.java
+++ src/main/java/org/example/services/OpenService.java
@@ -5,18 +5,17 @@ import org.example.databases.mongo.reposistories.TextRepo;
 import org.example.databases.mongo.reposistories.TextWithAllWordCountRepo;
 import org.example.databases.mongo.templates.KeywordCountTemplate;
 import org.example.models.*;
-import org.example.utils.counter.ICounter;
-import org.example.utils.counter.StackCounter;
-import org.example.utils.counter.UniqueCounter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 
 @Service
 public class OpenService {
+    private static final Logger LOGGER_OPEN_SERVICE = LoggerFactory.getLogger(OpenService.class);
     private final ParagraphRepo paragraphRepo;
     private final KeywordCountTemplate template;
     private final CommonKeywords commonKeywords;
@@ -54,39 +53,47 @@ public class OpenService {
         final long MINIMUM_SEARCH_KEY = 3;
         final long MAX_RESULT = 10;
 
-        // get a count
-        var listWordCount = textWithAllWordCountRepo.findFirst().getKeywordCounts();
+        var optionalTextWithAllWordCount = textWithAllWordCountRepo.findFirstWithKeywordCount();
+        if (optionalTextWithAllWordCount.isEmpty()) {
+            LOGGER_OPEN_SERVICE.error("Database can't find keywordCounts, search reduce is going to be performed");
+        } else {
+            // get a count
+            var listWordCount = optionalTextWithAllWordCount.get().getKeywordCounts();
 
-        // reduce to list of common words
-        List<String> commonWords = listWordCount
-                // use c2 - c1 to get descending order
-                .stream().sorted((c1, c2) -> {
-                    long res = c2.getCount() - c1.getCount();
-                    if (res > 0) return 1;
-                    if (res < 0) return -1;
-                    return 0;
-                })
-                // only get most common words
-                .limit(((long) listWordCount.size() * COMMON_PERCENTAGE) / 100)
-                // map to a list string
-                .map(KeywordCount::getWord)
-                .toList();
+            // reduce to list of common words
+            List<String> commonWords = listWordCount
+                    // use c2 - c1 to get descending order
+                    .stream().sorted((c1, c2) -> {
+                        long res = c2.getCount() - c1.getCount();
+                        if (res > 0) return 1;
+                        if (res < 0) return -1;
+                        return 0;
+                    })
+                    // only get most common words
+                    .limit(((long) listWordCount.size() * COMMON_PERCENTAGE) / 100)
+                    // map to a list string
+                    .map(KeywordCount::getWord)
+                    .toList();
 
-        // remove every words that is common
-        List<String> splitText = new java.util.ArrayList<>(Arrays.stream(text.split(" ")).toList());
+            // remove every words that is common
+            List<String> splitText = new java.util.ArrayList<>(Arrays.stream(text.split(" ")).toList());
 
-        // if key-phrase to short, just search it
-        if (splitText.size() > MINIMUM_SEARCH_KEY) {
-            remove_common:
-            for (var word : commonWords) {
-                // remove all that word
-                while (splitText.remove(word)) {
-                    if (splitText.size() <= MINIMUM_SEARCH_KEY) break remove_common;
+            // if key-phrase to short, just search it
+            if (splitText.size() > MINIMUM_SEARCH_KEY) {
+                remove_common:
+                for (var word : commonWords) {
+                    // remove all that word
+                    while (splitText.remove(word)) {
+                        if (splitText.size() <= MINIMUM_SEARCH_KEY) break remove_common;
+                    }
                 }
             }
+
+            text = String.join(" ", splitText);
         }
 
+
         // perform search
-        return textWithAllWordCountRepo.searchFullText(String.join(" ", splitText), 10);
+        return textWithAllWordCountRepo.searchFullText(text, 10);
     }
 }
